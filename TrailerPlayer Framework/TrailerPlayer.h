8a54893eb81561fdcf3c70a770ab4bb627f1aa0d
Abe Wang <abewang@kkstream.com>
2021-11-18 03:21:55 +0800

    feat: support Carthage.

---
+++ TrailerPlayer Framework/TrailerPlayer.h
@@ -0,0 +1,18 @@
+//
+//  TrailerPlayer.h
+//  TrailerPlayer Framework
+//
+//  Created by Abe Wang on 2021/11/18.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TrailerPlayer_Framework.
+FOUNDATION_EXPORT double TrailerPlayer_FrameworkVersionNumber;
+
+//! Project version string for TrailerPlayer_Framework.
+FOUNDATION_EXPORT const unsigned char TrailerPlayer_FrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TrailerPlayer_Framework/PublicHeader.h>
+
+
